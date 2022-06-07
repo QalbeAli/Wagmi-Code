0b8aac2fa90c607caf9e986510d37a86a8b3dc87
muhammadwajid <muhammad.wajid.shahid@gmail.com>
2022-06-07 17:07:23 +0500

    added history

--- components/table.js
+++ components/table.js
@@ -1,85 +1,131 @@
+import moment from 'moment';
+import { cards } from '../constants';
+import useStake from '../hooks/useStake';
 import tablestyles from '../styles/Table.module.css';
 const Table = () => {
     return (
         <>
 
-                <div className={tablestyles.table1}>
-                    <table className={tablestyles.tableBorder}>
-                        <thead>
-                            <tr className={tablestyles.tableRow1}>
-                              
-                                    <th  className={tablestyles.th1}>
-                                        <div >Stake Date</div></th>
-                                    <th  className={tablestyles.th1}>Pool Size</th>
-                                    <th className={tablestyles.th1} >Stake Amount</th>
-                                    <th className={tablestyles.th1} >Rewards</th>
-                                    <th className={tablestyles.th1}>Status</th>
-                             
+            <div className={tablestyles.table1}>
+                <table className={tablestyles.tableBorder}>
+                    <thead>
+                        <tr className={tablestyles.tableRow1}>
 
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr className={tablestyles.borderRow}>
+                            <th className={tablestyles.th1}>
+                                <div >Stake Date</div></th>
+                            <th className={tablestyles.th1}>Pool Size</th>
+                            <th className={tablestyles.th1} >Stake Amount</th>
+                            <th className={tablestyles.th1} >Rewards</th>
+                            <th className={tablestyles.th1}>Status</th>
 
-                                <td className={tablestyles.td1}>26 May 2022 </td>
-                                <td  className={tablestyles.td1}>Platinum Tier</td>
-                                <td  className={tablestyles.td1}>@3,000 $GEMS</td>
 
-                                <td className={tablestyles.td1}>360 $GEMS</td>
-                                <td  className={tablestyles.td1}><span className={tablestyles.completed}>Completed</span></td>
-                            </tr>
-                            <tr className={tablestyles.borderRow}>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <tr className={tablestyles.borderRow}>
 
-                                <td className={tablestyles.td1}>26 May 2022 </td>
-                                <td className={tablestyles.td1}>Platinum Tier</td>
-                                <td className={tablestyles.td1}>@3,000 $GEMS</td>
+                            <td className={tablestyles.td1}>26 May 2022 </td>
+                            <td className={tablestyles.td1}>Platinum Tier</td>
+                            <td className={tablestyles.td1}>@3,000 $GEMS</td>
 
-                                <td className={tablestyles.td1}>360 $GEMS</td>
-                                <td className={tablestyles.td1}><span className={tablestyles.completed}>Completed</span></td>
-                            </tr>
-                            <tr className={tablestyles.borderRow}>
+                            <td className={tablestyles.td1}>360 $GEMS</td>
+                            <td className={tablestyles.td1}><span className={tablestyles.completed}>Completed</span></td>
+                        </tr>
+                        <tr className={tablestyles.borderRow}>
 
-                                <td className={tablestyles.td1}>26 May 2022 </td>
-                                <td className={tablestyles.td1}>Gold Tier</td>
-                                <td className={tablestyles.td1}>@3,000 $GEMS</td>
+                            <td className={tablestyles.td1}>26 May 2022 </td>
+                            <td className={tablestyles.td1}>Platinum Tier</td>
+                            <td className={tablestyles.td1}>@3,000 $GEMS</td>
 
-                                <td className={tablestyles.td1}>540 $GEMS</td>
-                                <td className={tablestyles.td1} ><span className={tablestyles.up}>Locked-Up</span></td>
-                            </tr>
-                            <tr className={tablestyles.borderRow}>
+                            <td className={tablestyles.td1}>360 $GEMS</td>
+                            <td className={tablestyles.td1}><span className={tablestyles.completed}>Completed</span></td>
+                        </tr>
+                        <tr className={tablestyles.borderRow}>
 
-                                <td className={tablestyles.td1}>26 May 2022 </td>
-                                <td className={tablestyles.td1}>Silver Tier</td>
-                                <td className={tablestyles.td1}>@3,000 $GEMS</td>
+                            <td className={tablestyles.td1}>26 May 2022 </td>
+                            <td className={tablestyles.td1}>Gold Tier</td>
+                            <td className={tablestyles.td1}>@3,000 $GEMS</td>
 
-                                <td className={tablestyles.td1}>360 $GEMS</td>
-                                <td className={tablestyles.td1}><span className={tablestyles.going}>On Going</span></td>
-                            </tr>
-                            <tr className={tablestyles.borderRow}>
+                            <td className={tablestyles.td1}>540 $GEMS</td>
+                            <td className={tablestyles.td1} ><span className={tablestyles.up}>Locked-Up</span></td>
+                        </tr>
+                        <tr className={tablestyles.borderRow}>
 
-                                <td className={tablestyles.td1}>26 May 2022 </td>
-                                <td className={tablestyles.td1}>Gold Tier</td>
-                                <td className={tablestyles.td1}>@3,000 $GEMS</td>
+                            <td className={tablestyles.td1}>26 May 2022 </td>
+                            <td className={tablestyles.td1}>Silver Tier</td>
+                            <td className={tablestyles.td1}>@3,000 $GEMS</td>
 
-                                <td className={tablestyles.td1}>540 $GEMS</td>
-                                <td className={tablestyles.td1}><span className={tablestyles.up}>Locked-Up</span></td>
-                            </tr>
-                            <tr >
+                            <td className={tablestyles.td1}>360 $GEMS</td>
+                            <td className={tablestyles.td1}><span className={tablestyles.going}>On Going</span></td>
+                        </tr>
+                        <tr className={tablestyles.borderRow}>
 
-                                <td  className={tablestyles.td1}>26 May 2022 </td>
-                                <td  className={tablestyles.td1}>Platinum Tier</td>
-                                <td  className={tablestyles.td1}>@3,000 $GEMS</td>
+                            <td className={tablestyles.td1}>26 May 2022 </td>
+                            <td className={tablestyles.td1}>Gold Tier</td>
+                            <td className={tablestyles.td1}>@3,000 $GEMS</td>
 
-                                <td  className={tablestyles.td1}>360 $GEMS</td>
-                                <td className={tablestyles.td1}><span className={tablestyles.completed}>Completed</span></td>
-                            </tr>
+                            <td className={tablestyles.td1}>540 $GEMS</td>
+                            <td className={tablestyles.td1}><span className={tablestyles.up}>Locked-Up</span></td>
+                        </tr>
+                        <tr >
 
-                        </tbody>
-                    </table>
+                            <td className={tablestyles.td1}>26 May 2022 </td>
+                            <td className={tablestyles.td1}>Platinum Tier</td>
+                            <td className={tablestyles.td1}>@3,000 $GEMS</td>
 
-                </div>
-            
+                            <td className={tablestyles.td1}>360 $GEMS</td>
+                            <td className={tablestyles.td1}><span className={tablestyles.completed}>Completed</span></td>
+                        </tr> */}
+                        {
+                            cards.map(card => {
+                                return <TableHistory card={card} key={card.tierId} />
+                            })
+                        }
+                    </tbody>
+                </table>
+
+            </div>
+
         </>
     )
 }
 export default Table;
+
+function TableHistory({ card }) {
+    const { tierId, cardText } = card;
+    const {
+        ISLOCKED,
+        unlockTime,
+        depositTime,
+        stakedAmount,
+        reward
+    } = useStake(tierId)
+
+    return (
+        stakedAmount > 0 ?
+            <tr >
+
+                <td className={tablestyles.td1}>{depositTime} </td>
+                <td className={tablestyles.td1}>{cardText}</td>
+                <td className={tablestyles.td1}>@{Number(stakedAmount)} $GEMS</td>
+
+                <td className={tablestyles.td1}>{reward} $GEMS</td>
+                <td className={tablestyles.td1}>
+                    {
+                        ISLOCKED &&
+                        <span className={tablestyles.up}>Locked-Up</span>
+                    }
+                    {
+                        !ISLOCKED && moment(unlockTime).isBefore(moment()) &&
+                        <span className={tablestyles.completed}>Completed</span>
+                    }
+                    {
+                        !ISLOCKED && !moment(unlockTime).isBefore(moment()) &&
+                        <span className={tablestyles.going}>On Going</span>
+                    }
+
+                </td>
+            </tr>
+            : null
+    )
+}
--- constants/index.js
+++ constants/index.js
@@ -17,3 +17,26 @@ export const STAKING_TOKEN_ARGS = {
 }
 
 export { STAKING_CONTRACT_ABI };
+
+export const cards = [
+    {
+        tierId: 1,
+        status: "", cardText: "Bronze Tier", stackPeriod: "3 Months", LockUpPeriod: "2 Weeks", APY: "12%",
+        date1: "", date2: "", balance: "0.00 $GEMS", reward: "0.00 $GEMS"
+    },
+    {
+        tierId: 2,
+        status: "", cardText: "Silver Tier", stackPeriod: "6 Months", LockUpPeriod: "4 Weeks", APY: "15%",
+        date1: "May 24 12:00am UTC", date2: "August  24 12:00am UTC", balance: "3000 $GEMS", reward: "450 $GEMS"
+    },
+    {
+        tierId: 3,
+        status: "", cardText: "Gold Tier", stackPeriod: "9 Months", LockUpPeriod: "6 Weeks", APY: "18%",
+        date1: "May 24 12:00am UTC", date2: "August  24 12:00am UTC", balance: "3000 $GEMS", reward: "540 $GEMS"
+    },
+    {
+        tierId: 4,
+        status: "", cardText: "Platinum Tier", stackPeriod: "12 Months", LockUpPeriod: "8 Weeks", APY: "21%",
+        date1: "May 24 12:00am UTC", date2: "August  24 12:00am UTC", balance: "3000 $GEMS", reward: "630 $GEMS"
+    }
+]
